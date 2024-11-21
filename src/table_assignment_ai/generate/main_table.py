2c866eb30db97122e84c74e97fd4ff235afb30b4
hiroaki222 <hiroaki.takahara.222@gmail.com>
2024-11-21 20:01:40 +0900

    feat: Dataset generator

--- src/table_assignment_ai/generate/main_table.py
+++ src/table_assignment_ai/generate/main_table.py
@@ -1,38 +1,66 @@
-import pandas as pd
-import numpy as np
 from datetime import datetime, timedelta
 import uuid
 import random
+import json
+from faker import Faker
 
-np.random.seed(42)
+fake = Faker('jp-JP')
+Faker.seed(13413245)
 
-n = 1000
+n = 3
 
-uuids = [str(uuid.uuid4()) for _ in range(n)]
+today = datetime(2018, 7, 29, 23, 59, 59)
 
-datetimes = [datetime(2024, 12, 31, 23, 59, 59) - timedelta(
-    days=random.randint(0, 365),
-    hours=random.randint(0, 23),
-    minutes=random.randint(0, 59)
-) for _ in range(n)]
-
-weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
-def group_size():
+def fake_composition():
+    composition = []
     if random.random() < 0.95:
-        return random.randint(1,4)
+        num = random.randint(1,4)
     else:
-        return random.randint(5, 10)
+        num = random.randint(5, 10)
+    for i in range(num):
+        fake_user = fake.profile()
+        birthdate = fake_user['birthdate']
+        age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+        composition.append({
+            'age': age,
+            'gender': fake_user['sex']
+        })
+    return composition
 
-group_sizes = [group_size() for _ in range(n)]
+cafe_menu = [
+    {'emoji': '☕', 'name': 'コーヒー'},
+    {'emoji': '🍵', 'name': 'お茶'},
+    {'emoji': '🥛', 'name': '牛乳'},
+    {'emoji': '🧃', 'name': 'ジュース'},
+    {'emoji': '🧋', 'name': 'バブルティー'},
+    {'emoji': '🍰', 'name': 'ショートケーキ'},
+    {'emoji': '🎂', 'name': 'バースデーケーキ'},
+    {'emoji': '🧁', 'name': 'カップケーキ'},
+    {'emoji': '🍪', 'name': 'クッキー'},
+    {'emoji': '🍫', 'name': 'チョコレート'},
+    {'emoji': '🍩', 'name': 'ドーナツ'},
+    {'emoji': '🍦', 'name': 'ソフトクリーム'},
+    {'emoji': '🍨', 'name': 'アイスクリーム'},
+    {'emoji': '🥪', 'name': 'サンドイッチ'},
+    {'emoji': '🥗', 'name': 'サラダ'},
+    {'emoji': '🥐', 'name': 'クロワッサン'},
+    {'emoji': '🥯', 'name': 'ベーグル'},
+]
 
-df = pd.DataFrame({
-    'UID': uuids,
-    'DateTime': datetimes,
-    'DayOfWeek': [d.strftime('%A') for d in datetimes],
-    'NumberOfPeople': group_sizes
-})
+data = {i: {
+    "uuid": f'{uuid.uuid4()}',
+    "visit_time": (datetime(2018, 7, 29, 23, 59, 59) - timedelta(
+        hours=random.randint(0, 23),
+        minutes=random.randint(0, 59)
+    )).time().isoformat(),
+    "departure_time": "2023-11-20T16:15:00",
+    'group_composition': (group_composition := fake_composition()),
+    "ordered_items": [
+    dict(cafe_menu[random.randint(0, len(cafe_menu) - 1)],
+         quantity=random.randint(1, len(group_composition)))
+    for _ in range(len(group_composition))
+]
+    } for i in range(n)}
 
-df['DateTime'] = df['DateTime'].dt.strftime('%Y-%m-%dT%H:%M:%S')
-
-df.to_csv('./data/main_table_data.csv', index=False)
+with open('../../../data/main_dataset.json', 'w') as f:
+    json.dump(data, f, indent=2, ensure_ascii=False,)

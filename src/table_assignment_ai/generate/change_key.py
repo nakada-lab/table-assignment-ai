59755e8dfe7f3d595ce48fd0974b37688354b4f0
hiroaki222 <hiroaki.takahara.222@gmail.com>
2024-11-26 15:01:14 +0900

    `departure_time` all have the same value Fixes #4

--- src/table_assignment_ai/generate/change_key.py
+++ src/table_assignment_ai/generate/change_key.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import json
 from operator import itemgetter
 
-data = json.load(open('../../../data/updated_dataset.json', 'r'))
+data = json.load(open('../../../data/updated_dataset_processed.json.json', 'r'))
 
 data_list = list(data.values())
 
@@ -11,7 +11,7 @@ sorted_data = sorted(data_list, key=itemgetter('visit_time'))
 modded_data = [{
     "uuid": sorted_data[i]["uuid"],
     "visit_time": sorted_data[i]["visit_time"],
-    "departure_time": "00:30:59",
+    "departure_time": sorted_data[i]["departure_time"],
     "duration": int((datetime.strptime(sorted_data[i]["departure_time"], "%H:%M:%S") - datetime.strptime(sorted_data[i]["visit_time"], "%H:%M:%S")).total_seconds()),
     "group_composition": sorted_data[i]["group_composition"]
 } for i in range(len(sorted_data))]

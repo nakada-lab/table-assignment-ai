49fc17659d9f3e3e56670d84bcb296cde1521a45
hiroaki222 <hiroaki.takahara.222@gmail.com>
2024-11-27 19:00:18 +0900

    feat: Generate dataset

--- src/table_assignment_ai/generate/change_key.py
+++ src/table_assignment_ai/generate/change_key.py
@@ -2,19 +2,26 @@ from datetime import datetime
 import json
 from operator import itemgetter
 
-data = json.load(open('../../../data/updated_dataset_processed.json.json', 'r'))
+data = json.load(open("../../../data/updated_dataset.json", "r"))
 
-data_list = list(data.values())
+sorted_data = sorted(data, key=itemgetter("visit_time"))
 
-sorted_data = sorted(data_list, key=itemgetter('visit_time'))
+modded_data = [
+    {
+        "uuid": sorted_data[i]["uuid"],
+        "visit_time": sorted_data[i]["visit_time"],
+        "departure_time": sorted_data[i]["departure_time"],
+        "duration": int(
+            (
+                datetime.strptime(sorted_data[i]["departure_time"], "%H:%M:%S")
+                - datetime.strptime(sorted_data[i]["visit_time"], "%H:%M:%S")
+            ).total_seconds()
+        ),
+        "group_composition": sorted_data[i]["group_composition"],
+        "ordered_items": sorted_data[i]["ordered_items"],
+    }
+    for i in range(len(sorted_data))
+]
 
-modded_data = [{
-    "uuid": sorted_data[i]["uuid"],
-    "visit_time": sorted_data[i]["visit_time"],
-    "departure_time": sorted_data[i]["departure_time"],
-    "duration": int((datetime.strptime(sorted_data[i]["departure_time"], "%H:%M:%S") - datetime.strptime(sorted_data[i]["visit_time"], "%H:%M:%S")).total_seconds()),
-    "group_composition": sorted_data[i]["group_composition"]
-} for i in range(len(sorted_data))]
-
-with open('../../../data/tmp.json', 'w') as f:
+with open("../../../data/dataset.json", "w") as f:
     json.dump(modded_data, f, indent=2, ensure_ascii=False)

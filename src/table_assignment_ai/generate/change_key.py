ce50f6ccbc6815e54c334b3d3956eff4d50ea5a9
hiroaki222 <hiroaki.takahara.222@gmail.com>
2024-12-15 16:41:57 +0900

    feat: Changed some keys

--- src/table_assignment_ai/generate/change_key.py
+++ src/table_assignment_ai/generate/change_key.py
@@ -2,14 +2,13 @@ from datetime import datetime
 import json
 from operator import itemgetter
 
-data = json.load(open("../../../data/updated_dataset.json", "r"))
+data = json.load(open("../../../data/dataset.json", "r"))
 
 sorted_data = sorted(data, key=itemgetter("visit_time"))
 
-modded_data = [
-    {
+modded_data = {
+    sorted_data[i]["visit_time"]: {
         "uuid": sorted_data[i]["uuid"],
-        "visit_time": sorted_data[i]["visit_time"],
         "departure_time": sorted_data[i]["departure_time"],
         "duration": int(
             (
@@ -21,7 +20,7 @@ modded_data = [
         "ordered_items": sorted_data[i]["ordered_items"],
     }
     for i in range(len(sorted_data))
-]
+}
 
-with open("../../../data/dataset.json", "w") as f:
+with open("../../../data/update_dataset.json", "w") as f:
     json.dump(modded_data, f, indent=2, ensure_ascii=False)
